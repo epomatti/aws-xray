d79a4201e931dd30c90ff331266d5ba609f2410d
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-22 20:04:00 -0300

    go app xray

--- app/main.go
+++ app/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/aws/aws-xray-sdk-go/awsplugins/ecs"
+	"github.com/aws/aws-xray-sdk-go/xray"
+)
+
+func init() {
+	// conditionally load plugin
+	if os.Getenv("ENVIRONMENT") == "production" {
+		ecs.Init()
+	}
+
+	xray.Configure(xray.Config{
+		// ServiceVersion: "1.2.3",
+	})
+}
+
+func main() {
+	port := getPort()
+
+	http.Handle("/", xray.Handler(xray.NewFixedSegmentNamer("MyApp"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello!"))
+	})))
+
+	log.Println("Running on port " + port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8888"
+	}
+	return port
 }
