37b7940a3b9032f546a14c5ba5dea75aaee5a116
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-23 16:56:02 -0300

    xray sdk

--- app-nodejs/src/index.ts
+++ app-nodejs/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import AWSXRay from 'aws-xray-sdk';
+
+
 const app = express()
 const port = process.env.PORT || 3000
+
+AWSXRay.config([AWSXRay.plugins.EC2Plugin, AWSXRay.plugins.EC2Plugin]);
+app.use(AWSXRay.express.openSegment('MyApp'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(AWSXRay.express.closeSegment());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
